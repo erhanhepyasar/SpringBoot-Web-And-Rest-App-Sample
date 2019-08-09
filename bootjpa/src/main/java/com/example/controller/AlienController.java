fee5d2941673268c5bea1d64c7d22419ed6e2222
ERHAN <erhan.hepyasar@gmail.com>
2019-08-09 15:34:51 +0300

    Added controller methods for PUT, POST and DELETE

--- bootjpa/src/main/java/com/example/controller/AlienController.java
+++ bootjpa/src/main/java/com/example/controller/AlienController.java
@@ -4,21 +4,29 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.dao.AlienRepo;
 import com.example.model.Alien;
 
-@Controller
+@RestController
 public class AlienController {
 	
 	@Autowired
 	AlienRepo repo;
+	
+/////////////////////////////////////
+// 	Web (Jsp) Methods
+////////////////////////////////////
 	
 	@RequestMapping("/")
 	public String home() {
@@ -26,8 +34,8 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	@RequestMapping("/addAlien")
-	public String addAlien(Alien alien) {
+	@RequestMapping("/addAlienFromWeb")
+	public String addAlienFromWeb(Alien alien) {
 		
 		repo.save(alien);
 		
@@ -58,22 +66,47 @@ public class AlienController {
 		return "home.jsp";
 	}
 	
-	/////////////////////////////////////
-	// 	Rest Services
-	////////////////////////////////////
+/////////////////////////////////////
+// 	Rest Service Methods
+////////////////////////////////////
 	
-	@RequestMapping("/aliens")
-	@ResponseBody
-	public Iterable<Alien> getAliens() {
+	//@RequestMapping(path="/aliens", produces= {"application/xml"})
+	//@RequestMapping("/aliens")
+	//@ResponseBody
+	@GetMapping("/aliens")
+	public List<Alien> getAliens() {
 		
 		return repo.findAll();
 	}
 	
 	
-	@RequestMapping("/alien/{aid}")
-	@ResponseBody
+//	@RequestMapping("/alien/{aid}")
+//	@ResponseBody
+	@GetMapping("/alien/{aid}")
 	public Optional<Alien> getAlien(@PathVariable("aid") int aid) {
 		
 		return repo.findById(aid);
 	}
+	
+//	@PostMapping(path="/alien", consumes= {"application/json"})
+	@PostMapping("/alien")
+	public Alien addAlien(@RequestBody Alien alien) {		
+		repo.save(alien);		
+		return alien;
+	}
+	
+	@PutMapping("/alien")
+	public Alien saveOrUpdateAlien(@RequestBody Alien alien) {		
+		repo.save(alien);		
+		return alien;
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	public String deleteAlien(@PathVariable int aid) {
+		Alien alien = repo.getOne(aid);
+		repo.delete(alien);
+		return "deleted";
+	}
+	
+	
 }
--- bootjpa/src/main/java/com/example/dao/AlienRepo.java
+++ bootjpa/src/main/java/com/example/dao/AlienRepo.java
@@ -2,12 +2,12 @@ package com.example.dao;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import com.example.model.Alien;
 
-public interface AlienRepo extends CrudRepository<Alien, Integer> {
+public interface AlienRepo extends JpaRepository<Alien, Integer> {
 	
 	List<Alien> findByTech(String tech);
 	
